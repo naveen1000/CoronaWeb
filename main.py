db44c87dc2edf1334951e51131a31809f92e13ce
naveen1000 <loki.naveen1000@gmail.com>
2020-04-06 12:29:05 +0530

    added some feats1

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import requests
-app = Flask(__name__)
 
+def notify(msg):
+    #Telegam Bot Url
+    #url='https://api.telegram.org/bot879982304:AAHG7ZRyEMWoQB-ToaiJBv_gMvkW-ekJcSg/sendMessage?chat_id=582942300&text='+msg
+    #TelegramChannel chatId -1001181667975
+    url='https://api.telegram.org/bot831624998:AAFUKPiuCHJkO4kf75UHPbgMpN8M9yDo9ns/sendMessage?chat_id=582942300&parse_mode=Markdown&text='+msg
+    requests.get(url)
+    print("notified")
+
+
+app = Flask(__name__)
 raw = requests.get('https://api.covid19india.org/data.json')
 data = raw.json()
 daily_report = data['cases_time_series'][-1]
@@ -22,7 +31,9 @@ districtData = data['Andhra Pradesh']['districtData']
 
 @app.route('/')
 def hello_name():
-   return render_template('hello.html', result =[daily_report,statewise,districtData])
+   request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+   notify(request.remote_addr)  
+   return render_template('hello.html', result =[daily_report,statewise,districtData,request.remote_addr])
 
 @app.route('/states')
 def states():
